93a7a1031d4ef95dedb75f6f702e2e03b72ba017
nicegraphic <fnxa@posteo.de>
2022-03-30 21:04:14 +0200

    Added tests for reverse.

--- algoDat_impl_test/TestSequenceUtils.cs
+++ algoDat_impl_test/TestSequenceUtils.cs
@@ -18,6 +18,26 @@ public class TestSequenceUtilis
             Assert.Equal(currentCase.Expected, currentCase.ToSwap);
         }
     }
+
+    [Fact]
+    public void TestReverse()
+    {
+        for (int i = 0; i < TestCasesReverse.Length; i++)
+        {
+            int[] given = TestCasesReverse[i].given;
+        
+            int[] expected = new int[given.Length];
+            Array.Copy(given, expected, given.Length);
+            Array.Reverse(expected);
+        
+            SequenceUtils.InPlaceReverse(given);
+        
+            Assert.Equal(expected, given);
+        }
+    }
+
+    private record TestCaseReverse(int[] given);
+
     private record  TestCaseSwap(int[] ToSwap, int Left, int Right, int[] Expected);
 
     private static readonly TestCaseSwap[] TestCasesSwap = new TestCaseSwap[]
@@ -25,5 +45,10 @@ public class TestSequenceUtilis
         new(new int[] { 2, 4 }, 0, 1, new int[] { 4, 2 }),
         new(new int[] { 2, 4 }, 0, 0, new int[] { 2, 4 }),
         new(new int[] { 2, 4, 8, 7 }, 0, 3, new int[] { 7, 4, 8, 2 }) 
+    };
+
+    private static readonly TestCaseReverse[] TestCasesReverse = new TestCaseReverse[]
+    {
+        new(new[] { 1, 3, 5, 7 })
     };
 }
